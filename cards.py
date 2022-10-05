96567aaa6cfb531a762eba9ecee454f58c63de17
Gordon Gustafson <gordon.gustafson@SM-C02CD2PMMD6R.local>
2022-10-04 20:31:10 -0700

    Refactor to use dicts for CardCounts, add Effect class

--- cards.py
+++ cards.py
@@ -1,10 +1,31 @@
-import numpy as np
-import pandas as pd
+import random
 
 from typing import NamedTuple, Tuple, List, Dict
 
 
-CardCounts = np.ndarray
+# TODO: make this a proper Python enum
+class EFFECT_NAME:
+    DRAW_CARDS = "draw_cards"
+    PRODUCE_MONEY = "produce_money"
+    VP = "vp"
+
+Effect = NamedTuple("Effect", [
+    ("name", EFFECT_NAME),
+    ("value", int),
+])
+
+Card = NamedTuple("Card", [
+    ("name", str),
+    ("cost", int),
+    ("action_effects", Tuple[Effect]),
+    ("treasure_effects", Tuple[Effect]),
+    ("vp_effects", Tuple[Effect]),
+])
+
+def make_card(name, cost, action_effects=(), treasure_effects=(), vp_effects=()):
+    return Card(name, cost, action_effects, treasure_effects, vp_effects)
+
+CardCounts = Dict[Card, int]
 
 _PlayerBase = NamedTuple("Player", [
     ("hand", CardCounts),
@@ -66,25 +87,26 @@ Action = NamedTuple("Action", [
 ])
 
 # TODO: should we use the same "money_produced" field for both treasures and actions?
-_RAW_CARD_DEFS = [
-    {"name": "copper",   "cost": 0, "money_produced": 1, "type": "treasure"},
-    {"name": "silver",   "cost": 3, "money_produced": 2, "type": "treasure"},
-    {"name": "gold",     "cost": 6, "money_produced": 3, "type": "treasure"},
+CARD_DEFS = {
+    "copper":   make_card(name="copper",   cost=0, treasure_effects=(Effect(EFFECT_NAME.PRODUCE_MONEY, 1),)),
+    "silver":   make_card(name="silver",   cost=3, treasure_effects=(Effect(EFFECT_NAME.PRODUCE_MONEY, 2),)),
+    "gold":     make_card(name="gold",     cost=6, treasure_effects=(Effect(EFFECT_NAME.PRODUCE_MONEY, 3),)),
 
-    {"name": "estate",   "cost": 2, "victory_points": 1, "type": "victory"},
-    {"name": "duchy",    "cost": 5, "victory_points": 3, "type": "victory"},
-    {"name": "province", "cost": 8, "victory_points": 6, "type": "victory"},
+    "estate":   make_card(name="estate",   cost=2, vp_effects=(Effect(EFFECT_NAME.VP, 1),)),
+    "duchy":    make_card(name="duchy",    cost=5, vp_effects=(Effect(EFFECT_NAME.VP, 3),)),
+    "province": make_card(name="province", cost=8, vp_effects=(Effect(EFFECT_NAME.VP, 6),)),
+
+    "curse":   make_card(name="curse",   cost=0, vp_effects=(Effect(EFFECT_NAME.VP, -1),)),
 
     # +cards
-    # {"name": "Smithy",       "cost": 4, "draw_cards": 3,}
+    # {"name": "Smithy",       "cost": 4, EFFECT_NAME.DRAW_CARDS: 3,}
 
     # +actions
-    # {"name": "Laboratory",   "cost": 5, "type": "action", "draw_cards": 2, "actions": 1,}
-    # {"name": "Village",      "cost": 3, "type": "action", "draw_cards": 1, "actions": 2,}
+    # {"name": "Laboratory",   "cost": 5, "type": "action", EFFECT_NAME.DRAW_CARDS: 2, "actions": 1,}
+    # {"name": "Village",      "cost": 3, "type": "action", EFFECT_NAME.DRAW_CARDS: 1, "actions": 2,}
 
     # trashing cards
     # {"name": "Chapel",       "cost": 2, "type": "action", "trash_up_to_X_cards_from_your_hand": 4,
-    # {"name": "Sentry",       "cost": 5, "type": "action", "actions": 1, @"+1 card . Look at the top 2 cards of your deck. Trash and/or discard any number of them, put the rest back on top in any order"
 
     # gaining cards
     # {"name": "Workshop",     "cost": 3, "type": "action", @"gain_a_card_costing_up_to_4": 1
@@ -93,117 +115,150 @@ _RAW_CARD_DEFS = [
 
     # +buys
     # {"name": "Festival",     "cost": 5, "type": "action", "actions": 2, @", +1 buy, +2$"
-    # {"name": "Market",       "cost": 5, "type": "action", "draw_cards": 1, "actions": 1, @"+1$ +1 buy"
-    # {"name": "Council Room", "cost": 5, "type": "action", "draw_cards": 4, @"+1 buy, each other player drawns a card"
+    # {"name": "Market",       "cost": 5, "type": "action", EFFECT_NAME.DRAW_CARDS: 1, "actions": 1, @"+1$ +1 buy"
+    # {"name": "Council Room", "cost": 5, "type": "action", EFFECT_NAME.DRAW_CARDS: 4, @"+1 buy, each other player drawns a card"
+
+    # simple draw/discard effects
+    # {"name": "Cellar",       "cost": 2, "type": "action", "actions": 1, "discard_any_number_then_draw_that_many": 1,
+    # {"name": "Harbinger",    "cost": 3, "type": "action", EFFECT_NAME.DRAW_CARDS: 1, "actions": 1, "put_any_card_from_discard_pile_onto_deck": 1,
+    # {"name": "Poacher",      "cost": 4, "type": "action", EFFECT_NAME.DRAW_CARDS: 1, "actions": 1, @"+1$, discard a card per empty supply pile"
+
+    # interacting with other cards
+    # {"name": "Moneylender",  "cost": 4, "type": "action", @"you may trash a copper from your hand for +3$"
+    # {"name": "Merchant",     "cost": 3, "type": "action", EFFECT_NAME.DRAW_CARDS: 1, "actions": 1, "the_first_time_you_play_a_silver_this_turn_+1_money": 1,
 
     # VP cards
     # {"name": "Gardens",      "cost": 4, "type": "victory", @"worth 1 vp per 10 cards you have (rounded down)"
 
-    # simple draw/discard effects
-    # {"name": "Cellar",       "cost": 2, "type": "action", "actions": 1, "discard_any_number_then_draw_that_many": 1,
-    # {"name": "Harbinger",    "cost": 3, "type": "action", "draw_cards": 1, "actions": 1, "put_any_card_from_discard_pile_onto_deck": 1,
-    # {"name": "Poacher",      "cost": 4, "type": "action", "draw_cards": 1, "actions": 1, @"+1$, discard a card per empty supply pile"
-
     # attacks
-    # {"name": "Witch",        "cost": 5, "type": "action", "draw_cards": 2, @"each other player gains a curse"
-    # {"name": "Militia",      "cost": 4, "type": "action", @"+2$ each other player discards down to 3 cards in hand"
+    # {"name": "Witch",        "cost": 5, "type": "action", EFFECT_NAME.DRAW_CARDS: 2, @"each other player gains a curse"
     # {"name": "Bandit",       "cost": 5, "type": "action", @"gain a gold. each other player reveals the top 2 cards of their deck, trashes a revealed treasure other than copper, and discards the rest"
+    # {"name": "Militia",      "cost": 4, "type": "action", @"+2$ each other player discards down to 3 cards in hand"
     # {"name": "Bureaucrat",   "cost": 4, "type": "action", @"gain a silver onto your deck. each other player reveals a victory card from their hand it puts it onto their deck (or reveals a hand with no victory cards)"
-
-    # interacting with other cards
-    # {"name": "Moneylender",  "cost": 4, "type": "action", @"you may trash a copper from your hand for +3$"
-    # {"name": "Merchant",     "cost": 3, "type": "action", "draw_cards": 1, "actions": 1, "the_first_time_you_play_a_silver_this_turn_+1_money": 1,
-    # {"name": "Moat",         "cost": 2, "type": "action", "draw_cards": 2, "moat_effect": 1,
+    # {"name": "Moat",         "cost": 2, "type": "action", EFFECT_NAME.DRAW_CARDS: 2, "moat_effect": 1,
 
     # new types of game state or choices
+    # {"name": "Sentry",       "cost": 5, "type": "action", "actions": 1, @"+1 card . Look at the top 2 cards of your deck. Trash and/or discard any number of them, put the rest back on top in any order"
     # {"name": "Throne Room",  "cost": 4, "type": "action", @"you may play an action card from your hand twice"
     # {"name": "Vassal",       "cost": 3, "type": "action", "money_produced": 2, "Discard_the_top_card_of_your_deck_if_it's_an_action_card,_you_may_play_it": 1,
     # {"name": "Artisan",      "cost": 6, "type": "action", @"gain a card to your hand costing up to $5. put a card from your hand onto your deck"
     # {"name": "Library",      "cost": 5, "type": "action", @"draw until you have 7 cards in hand, skipping any action cards you choose to. Set those aside, discarding them afterwards"
-]
+}
 
-CARD_DEFS = pd.DataFrame.from_dict(_RAW_CARD_DEFS).fillna(0)
-
-NUM_CARDS_DEFINED = CARD_DEFS.shape[0]
 
 def empty_card_counts():
-    return np.zeros(shape=(NUM_CARDS_DEFINED,), dtype=np.int32)
+    return {}
 
 def num_cards(card_counts: CardCounts) -> int:
-    return np.sum(card_counts)
+    return sum(card_counts.values())
 
 def card_counts_equal(lhs: CardCounts, rhs: CardCounts) -> bool:
-    return np.array_equal(lhs, rhs)
+    return ({c: f for c, f in lhs.items() if f > 0}
+            == {c: f for c, f in rhs.items() if f > 0})
 
-def card_name_to_index(card_name: str) -> int:
-    return CARD_DEFS.index[CARD_DEFS['name'] == card_name].item()
-
-def add_card(card_counts: CardCounts, card_index: int) -> CardCounts:
+def add_card(card_counts: CardCounts, card: Card) -> CardCounts:
     card_counts_copy = card_counts.copy()
-    card_counts_copy[card_index] += 1
+    if card in card_counts_copy:
+        card_counts_copy[card] += 1
+    else:
+        card_counts_copy[card] = 1
     return card_counts_copy
 
-def remove_card(card_counts: CardCounts, card_index: int) -> CardCounts:
-    if card_counts[card_index] < 1:
-        raise ValueError("Can't remove card index {card_index} from card counts {card_counts}")
+def remove_card(card_counts: CardCounts, card: Card) -> CardCounts:
+    if card not in card_counts or card_counts[card] < 1:
+        raise ValueError("Can't remove card {card} from card counts {card_counts}")
 
     card_counts_copy = card_counts.copy()
-    card_counts_copy[card_index] -= 1
+    card_counts_copy[card] -= 1
     return card_counts_copy
-
-def add_card_by_name(card_counts: CardCounts, card_name: str) -> CardCounts:
-    card_index = card_name_to_index(card_name)
-    return add_card(card_counts, card_index)
-
-def remove_card_by_name(card_counts: CardCounts, card_name: str) -> CardCounts:
-    card_index = card_name_to_index(card_name)
-    return remove_card(card_counts, card_index)
-
-def num_copies_of_card(card_counts: CardCounts, card_name: str) -> int:
-    card_index = card_name_to_index(card_name)
-    return card_counts[card_index]
 
 def dict_to_card_counts(card_names_dict: Dict[str, int]) -> CardCounts:
-    return np.array([card_names_dict.get(card_name, 0) for card_name in CARD_DEFS['name'].to_list()])
-
-def card_counts_to_dict(card_counts: CardCounts) -> Dict[str, int]:
-    return {card_name: num_copies_of_card(card_counts, card_name)
-            for card_name in CARD_DEFS['name'].to_list()
-            if num_copies_of_card(card_counts, card_name) > 0}
+    result = {}
+    for card_name, card_occurrences in card_names_dict.items():
+        result[CARD_DEFS[card_name]] = card_occurrences
+    return result
 
 
 def treasure_total(card_counts: CardCounts) -> int:
-    return CARD_DEFS.money_produced.dot(card_counts)
+    total = 0
+    for card, card_occurrences in card_counts.items():
+    # HACK this assumes all treasures only have one treasure effect
+        if len(card.treasure_effects) > 0:
+            total += card_occurrences * card.treasure_effects[0].value
+
+    return total
+
 
 def vp_total(card_counts: CardCounts) -> int:
-    return CARD_DEFS.victory_points.dot(card_counts)
+    total = 0
+    for card, card_occurrences in card_counts.items():
+    # HACK this assumes all victory cards only have one vp effect
+        if len(card.vp_effects) > 0:
+            total += card_occurrences * card.vp_effects[0].value
+
+    return total
 
 def add_card_counts(card_counts_lhs: CardCounts, card_counts_rhs: CardCounts) -> CardCounts:
-    return card_counts_lhs + card_counts_rhs
-
+    result = card_counts_lhs.copy()
+    for card, card_occurrences in card_counts_rhs.items():
+        if card in result:
+            result[card] += card_occurrences
+        else:
+            result[card] = card_occurrences
+    return result
 
 def total_player_vp(player: Player) -> int:
     return vp_total(player.hand) + vp_total(player.deck) + vp_total(player.discard_pile)
+
+
+################################################################################
+#                                                  Operations Using Card Names #
+################################################################################
+
+def card_name_to_card(card_name: str) -> Card:
+    return CARD_DEFS[card_name]
+
+def add_card_by_name(card_counts: CardCounts, card_name: str) -> CardCounts:
+    card = card_name_to_card(card_name)
+    return add_card(card_counts, card)
+
+def remove_card_by_name(card_counts: CardCounts, card_name: str) -> CardCounts:
+    card = card_name_to_card(card_name)
+    return remove_card(card_counts, card)
+
+def num_copies_of_card(card_counts: CardCounts, card_name: str) -> int:
+    card = card_name_to_card(card_name)
+    return card_counts[card]
+
+def card_counts_to_dict(card_counts: CardCounts) -> Dict[str, int]:
+    return {card.name: num_occurrences
+            for card, num_occurrences in card_counts.items()
+            if num_occurrences > 0}
+
+################################################################################
+#                                                                      Options #
+################################################################################
 
 # TODO: make this return a set? Will need to stop using List in GameState
 def buy_phase_options(buy_game_state: GameState) -> List[Action]:
     player = buy_game_state.current_player()
     total_money_for_turn = treasure_total(player.hand)
 
-    buyable_card_indices = CARD_DEFS.index[(CARD_DEFS['cost'] <= total_money_for_turn)
-                                           & (buy_game_state.supply > 0)].to_list()
+    supply = buy_game_state.supply
+    buyable_cards = [card for card in supply.keys()
+                     if supply[card] > 0
+                     and card.cost <= total_money_for_turn]
 
     cleanup_game_state = buy_game_state._replace(turn_phase=TURN_PHASES.CLEANUP)
     buy_nothing = Action(game_state=cleanup_game_state, description="buy nothing")
     actions = [buy_nothing]
-    for buyable_card_index in buyable_card_indices:
+    for buyable_card in buyable_cards:
         game_state = (cleanup_game_state
-                      ._replace(supply=remove_card(cleanup_game_state.supply, buyable_card_index))
+                      ._replace(supply=remove_card(supply, buyable_card))
                       .replace_current_player_kwargs(discard_pile=add_card(player.discard_pile,
-                                                                           buyable_card_index)))
+                                                                           buyable_card)))
 
-        card_name = CARD_DEFS["name"][buyable_card_index]
-        actions.append(Action(game_state, f"buy {card_name}"))
+        actions.append(Action(game_state, f"buy {buyable_card.name}"))
 
     return actions
 
@@ -216,7 +271,9 @@ def draw_card(player: Player) -> Player:
                                  discard_pile=empty_card_counts())
 
     deck = player.deck
-    card_drawn = np.random.choice(a=NUM_CARDS_DEFINED, size=1, replace=False, p=deck/num_cards(deck))
+    unique_cards, frequencies = zip(*deck.items())
+
+    card_drawn = random.choices(unique_cards, weights=frequencies, k=1)[0]
     player = player._replace(hand=add_card(player.hand, card_drawn),
                              deck=remove_card(deck, card_drawn))
 
@@ -304,7 +361,11 @@ def initial_game_state(num_players: int) -> GameState:
                      turn_phase=TURN_PHASES.ACTION)
 
 def game_completed(game_state: GameState) -> bool:
-    num_empty_piles = np.sum(game_state.supply == 0)
+    # distinguish between an card that has been fully bought up and a card that wasn't in the game
+    # HACK for now assumes all cards defined are in the game
+    original_non_empty_piles = len(CARD_DEFS)
+    current_non_empty_piles = len([True for card in CARD_DEFS if game_state.supply.get(card, 0) == 0])
+    num_empty_piles = original_non_empty_piles - current_non_empty_piles
     return (num_empty_piles >= 3
             or num_copies_of_card(game_state.supply, "province") == 0)
 
