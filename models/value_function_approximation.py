050200bd2d2076f09b2ebcb49fd00743d75b2beb
Gordon Gustafson <gordon3.14@gmail.com>
2025-02-04 18:03:47 -0800

    Move DominionModel into value_function_approximation.py

--- models/value_function_approximation.py
+++ models/value_function_approximation.py
@@ -1,5 +1,4 @@
 from pytorch.dataloader import DominionDataset, collate_fn
-from pytorch.model import DominionModel
 
 import torch
 from torch.utils.data import DataLoader
@@ -11,6 +10,38 @@ import play
 
 import pandas as pd
 
+
+class DominionModel(L.LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, batch, batch_idx):
+        features, reward = batch
+        raw_scores = self.model.forward(features)
+        return torch.nn.functional.sigmoid(raw_scores)
+
+    def training_step(self, batch, batch_idx):
+        predicted_rewards = self.forward(batch, batch_idx).squeeze(1)
+        features, rewards = batch
+        # print(f"predicted_rewards: {predicted_rewards}, rewards: {rewards}")
+        for parameter in self.model.parameters():
+            print(parameter.data)
+        train_loss = torch.nn.functional.binary_cross_entropy(predicted_rewards, target=rewards)
+        self.log("train_loss", train_loss)
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def on_validation_epoch_end(self) -> None:
+        pass
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.model.parameters(),
+                                 lr=1e-1,
+                                 betas=(0.9, 0.999),
+                                 weight_decay=0.04)
 
 def train_pytorch_model(games_df: pd.DataFrame, model, num_epochs: int, batch_size: int) -> None:
     dataset = DominionDataset(games_df)
